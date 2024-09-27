707e65f469c298e7e77ea0af896397916b5ba6eb
azaikova7 <142910314+azaikova7@users.noreply.github.com>
2024-09-27 14:13:41 +0500

    Add files via upload

---
+++ Destroy.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //уничтожение игрового объекта
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ crickets.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crickets : MonoBehaviour
+{
+    //команды ипсолняются только в начале игры
+    void Start()
+    {
+        //вывод в консоль слова "crickets"
+        print("crickets");
+    }
+    //команды, которые выполняются каждый кадр
+    void Update()
+    {
+        //вывод в консоль слова "crickets"
+        print("crickets");
+    }
+}
